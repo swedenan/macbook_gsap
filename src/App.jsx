c5377eace07ad3e55a43d97f84402fd5d22a950f
SwedeGeo <87441395+swedenan@users.noreply.github.com>
2025-11-07 19:27:04 +0300

    Implement hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-
 import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
 
 function App() {
-
-
   return (
-    <main>
-        <NavBar/>
+    <main className="h-[100vh]">
+      <NavBar />
+      <Hero />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from "react";
+
+const Hero = () => {
+  const videoRef = useRef();
+
+  useEffect(() => {
+    if (videoRef.current) videoRef.current.playbackRate = 2;
+  }, []);
+
+  return (
+    <section id="hero">
+      <div>
+        <h1>MacBook Pro</h1>
+        <img src="/title.png" alt="MacBook Title" />
+      </div>
+
+      <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playInline />
+
+      <button>Buy</button>
+
+      <p>From $1599 of 133/mo for 12 mouths</p>
+    </section>
+  );
+};
+export default Hero;
