582f4cca8fbf2fcff5802452b26328b4fe300504
SwedeGeo <87441395+swedenan@users.noreply.github.com>
2026-01-04 18:01:30 +0300

    Showcase section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,34 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef } from 'react'
 
 const Hero = () => {
-  const videoRef = useRef();
+	const videoRef = useRef()
 
-  useEffect(() => {
-    if (videoRef.current) videoRef.current.playbackRate = 2;
-  }, []);
+	useEffect(() => {
+		if (videoRef.current) videoRef.current.playbackRate = 2
+	}, [])
 
-  return (
-    <section id="hero">
-      <div>
-        <h1>MacBook Pro</h1>
-        <img src="/title.png" alt="MacBook Title" />
-      </div>
+	return (
+		<section id="hero">
+			<div>
+				<h1>MacBook Pro</h1>
+				<img
+					src="/title.png"
+					alt="MacBook Title"
+				/>
+			</div>
 
-      <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playInline />
+			<video
+				ref={videoRef}
+				src="/videos/hero.mp4"
+				autoPlay
+				muted
+				playInline
+			/>
 
-      <button>Buy</button>
+			<button>Buy</button>
 
-      <p>From $1599 of 133/mo for 12 mouths</p>
-    </section>
-  );
-};
-export default Hero;
+			<p>From $1599 of 133/mo for 12 mouths</p>
+		</section>
+	)
+}
+export default Hero
