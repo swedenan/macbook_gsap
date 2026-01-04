467b118cbbdcab4196c22a3456de39a6f0115f79
SwedeGeo <87441395+swedenan@users.noreply.github.com>
2026-01-04 18:24:25 +0300

    End

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import NavBar from "./components/NavBar.jsx";
-import Hero from "./components/Hero.jsx";
-import ProductViewer from "./components/ProductViewer.jsx";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
-import Showcase from "./components/Showcase.jsx";
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+import Features from './components/Features .jsx'
+import Footer from './components/Footer.jsx'
+import Hero from './components/Hero.jsx'
+import NavBar from './components/NavBar.jsx'
+import Performance from './components/Performance.jsx'
+import ProductViewer from './components/ProductViewer.jsx'
+import Showcase from './components/Showcase.jsx'
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger)
 
 function App() {
-  return (
-    <main>
-      <NavBar />
-      <Hero />
-      <ProductViewer />
-      <Showcase />
-    </main>
-  );
+	return (
+		<main>
+			<NavBar />
+			<Hero />
+			<ProductViewer />
+			<Showcase />
+			<Performance />
+			<Features />
+			<Footer />
+		</main>
+	)
 }
 
-export default App;
+export default App
